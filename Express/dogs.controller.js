eb371870a92e5a3b4f175237e9862894e6056e65
Michael A Chavez <machavez2011@gmail.com>
2018-05-07 20:40:58 -0700

    Update dogs.controller.js

--- Express/dogs.controller.js
+++ Express/dogs.controller.js
@@ -1,45 +1,83 @@
-function getByTiers(tier) {
-  return conn
-    .db()
-    .collection("challenges")
-    .aggregate([
-      {
-        $match: {
-          tier: { $in: tier } 
-        }
-      },
-      {
-        $lookup: {
-          from: "sponsors",
-          localField: "sponsorId",
-          foreignField: "_id",
-          as: "sponsorId"
-        }
-      },
-      {
-        $unwind: {
-          path: "$sponsorId",
-          preserveNullAndEmptyArrays: true
-        }
-      },
-      {
-        $project: {
-          "sponsorId.firstName": 0,
-          "sponsorId.lastName": 0,
-          "sponsorId.email": 0,
-          "sponsorId.zipCode": 0,
-          "sponsorId.phone": 0,
-          "sponsorId.createDate": 0,
-          "sponsorId.updateDate": 0
-        }
-      }
-    ])
-    .toArray()
-    .then(challenges => {
-      for (let i = 0; i < challenges.length; i++) {
-        let challenge = challenges[i];
-        challenge._id = challenge._id.toString();
-      }
-      return challenges;
+const responses = require("../models/responses");
+const dogsService = require("../services/dogs.service");
+const apiPrefix = "/api/dogs";
+
+module.exports = {
+  readAll: readAll,
+  readById: readById,
+  create: create,
+  update: update,
+  delete: _delete
+};
+
+function readAll(req, res) {
+  dogsService
+    .readAll()
+    .then(dogs => {
+      const responseModel = new responses.ItemsResponse();
+      responseModel.items = dogs;
+      res.json(responseModel);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(new responses.ErrorResponse(err));
+    });
+}
+
+function readById(req, res) {
+  dogsService
+    .readById(req.params.id)
+    .then(dogs => {
+      const responseModel = new responses.ItemResponse();
+      responseModel.item = dogs;
+      res.json(responseModel);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(new responses.ErrorResponse(err));
+    });
+}
+
+function create(req, res) {
+  dogsService
+    .create(req.model)
+    .then(id => {
+      const responseModel = new responses.ItemResponse();
+      responseModel.item = id;
+      res
+        .status(201)
+        .location(`${apiPrefix}/${id}`)
+        .json(responseModel);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(new responses.ErrorResponse(err));
+    });
+}
+
+function update(req, res) {
+  req.model.updateDate = new Date();
+  dogsService
+    .update(req.params.id, req.model)
+    .then(dogs => {
+      const responseModel = new responses.SuccessResponse();
+      res.status(200).json(responseModel);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(new responses.ErrorResponse(err));
+    });
+}
+
+function _delete(req, res) {
+  dogsService
+    .delete(req.params.id)
+    .then(() => {
+      const responseModel = new responses.SuccessResponse();
+      res.status(200).json(responseModel);
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(500).send(new responses.ErrorResponse(err));
     });
 }
